851294550429e08f5c1fbc295737fd074b8f1d6d
MayoG <dhtnthu9794048@gmail.com>
2020-02-27 11:10:42 +0200

    Add new messageHandler methods and their implementation in redisHandler

--- pipert/core/message_handlers.py
+++ pipert/core/message_handlers.py
@@ -16,6 +16,30 @@ class MessageHandler(ABC):
         pass
 
     @abstractmethod
+    def read_next_msg(self, in_key):
+        """
+        Reads the following message from the one that was last read,
+        if no message read before, reads the last message in the message broker.
+        cannot read the same message twice.
+
+        Args:
+            in_key: the name of the queue/stream at which the relevant message
+            is located.
+        """
+        pass
+
+    @abstractmethod
+    def read_most_recent_msg(self, in_key):
+        """
+        Reads the latest message in the message broker, cannot read the same message twice.
+
+        Args:
+            in_key: the name of the queue/stream at which the relevant message
+            is located.
+        """
+        pass
+
+    @abstractmethod
     def send(self, out_key, msg):
         """
         Sends a message to the message broker.
@@ -48,13 +72,43 @@ class RedisHandler(MessageHandler):
         self.conn = None
         self.url = url
         self.maxlen = maxlen
+        self.last_msg_id = None
         self.connect()
+
+    def read_next_msg(self, in_key):
+        if self.last_msg_id:
+            last_msg_id_to_read = self.last_msg_id.split("-")
+            last_msg_id_to_read = last_msg_id_to_read[0] + "-" + str(int(last_msg_id_to_read[1]) + 1)
+        else:
+            return self.receive(in_key)
+
+        redis_msg = self.conn.xrange(in_key, count=1, min=last_msg_id_to_read)
+        if not redis_msg:
+            return None
+        self.last_msg_id = redis_msg[0][0]
+        msg = redis_msg[0][1]["msg".encode("utf-8")]
+        return msg
+
+    def read_most_recent_msg(self, in_key):
+        if self.last_msg_id:
+            last_msg_id_to_read = self.last_msg_id.split("-")
+            last_msg_id_to_read = last_msg_id_to_read[0] + "-" + str(int(last_msg_id_to_read[1]) + 1)
+        else:
+            return self.receive(in_key)
+
+        redis_msg = self.conn.xrevrange(in_key, count=1, min=last_msg_id_to_read)
+        if not redis_msg:
+            return None
+        self.last_msg_id = redis_msg[0][0]
+        msg = redis_msg[0][1]["msg".encode("utf-8")]
+        return msg
 
     def receive(self, in_key):
         # TODO - refactor to use xread instead of xrevrange
         redis_msg = self.conn.xrevrange(in_key, count=1)
         if not redis_msg:
             return None
+        self.last_msg_id = redis_msg[0][0]
         msg = redis_msg[0][1]["msg".encode("utf-8")]
         return msg
 
