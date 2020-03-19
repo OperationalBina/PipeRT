3cb726bd87bb76416099512b0d1072dbe8a42c11
MayoG <dhtnthu9794048@gmail.com>
2020-03-19 23:57:30 +0200

    Add new tests to PipelineManager

--- tests/pipert/core/test_pipeline_manager.py
+++ tests/pipert/core/test_pipeline_manager.py
@@ -9,6 +9,57 @@ def pipeline_manager():
     return pipeline_manager
 
 
-def test_create_component(pipeline_manager):
+def test_create_component(pipeline_manager):  # cant add with the same name for queue comp and routine
     assert pipeline_manager.create_component("comp1")["Succeeded"]
     assert "comp1" in pipeline_manager.components
+
+
+def test_create_component_with_same_name(pipeline_manager):
+    assert pipeline_manager.create_component("comp1")["Succeeded"]
+    assert not pipeline_manager.create_component("comp1")["Succeeded"]
+
+
+def test_add_queue(pipeline_manager):
+    assert pipeline_manager.create_component("comp1")["Succeeded"]
+    assert pipeline_manager.create_queue_to_component("comp1", "que1")["Succeeded"]
+    assert "que1" in pipeline_manager.components["comp1"].queues
+
+
+def test_add_queue_with_same_name(pipeline_manager):
+    assert pipeline_manager.create_component("comp1")["Succeeded"]
+    assert pipeline_manager.create_queue_to_component("comp1", "que1")["Succeeded"]
+    assert not pipeline_manager.create_queue_to_component("comp1", "que1")["Succeeded"]
+
+
+def test_create_routine(pipeline_manager):
+    assert pipeline_manager.create_component("comp1")["Succeeded"]
+    assert pipeline_manager.create_queue_to_component("comp1", "que1")["Succeeded"]
+    assert pipeline_manager.add_routine_to_component(
+        component_name="comp1",
+        routine_type_name="ListenToStream",
+        stream_address="/home/internet/Desktop/video.mp4",
+        out_queue="que1",
+        fps=30,
+        name="capture_frame")["Succeeded"]
+
+    assert len(pipeline_manager.components["comp1"]._routines) == 1
+
+
+def test_create_routine_with_same_name(pipeline_manager):
+    assert pipeline_manager.create_component("comp1")["Succeeded"]
+    assert pipeline_manager.create_queue_to_component("comp1", "que1")["Succeeded"]
+    assert pipeline_manager.add_routine_to_component(
+        component_name="comp1",
+        routine_type_name="ListenToStream",
+        stream_address="/home/internet/Desktop/video.mp4",
+        out_queue="que1",
+        fps=30,
+        name="capture_frame")["Succeeded"]
+
+    assert not pipeline_manager.add_routine_to_component(
+        component_name="comp1",
+        routine_type_name="ListenToStream",
+        stream_address="/home/internet/Desktop/video.mp4",
+        out_queue="que1",
+        fps=30,
+        name="capture_frame")["Succeeded"]
