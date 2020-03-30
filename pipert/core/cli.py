b1ea4301f02e5989246d85e0ab08b61d31689dfd
MayoG <dhtnthu9794048@gmail.com>
2020-03-30 12:53:13 +0300

    Add clear console and check connection to the manager

--- pipert/core/cli.py
+++ pipert/core/cli.py
@@ -1,7 +1,18 @@
+import os
+
 import yaml
 import zerorpc
 from yaml.scanner import ScannerError
+import sys
+from subprocess import call
 
+EXIT_METHOD_TEXT = 'exit'
+SHOW_METHODS_TEXT = 'show_methods'
+LOAD_CONFIG_FILE_TEXT = "load_config_file"
+EXPORT_CONFIG_FILE_TEXT = "export_config_file"
+CLEAR_CONSOLE_TEXT = 'clear'
+
+clear_console = lambda: call('clear' if os.name == 'posix' else 'cls')
 
 def execute_method(method_name, connection):
     parameters_values = {}
@@ -12,40 +23,46 @@ def execute_method(method_name, connection):
         elif "kwargs" in parameter_name:
             user_input = input("Enter key - value for " + parameter_name + ". To stop enter stop: ")
             while user_input != "stop":
-                key, value = user_input.split("-")
-                parameters_values[key.strip()] = value.strip()
+                try:
+                    key, value = user_input.split("-")
+                    parameters_values[key.strip()] = value.strip()
+                except ValueError:
+                    print("Can't parse", user_input, "to key - value format")
                 user_input = input("Enter key - value for " + parameter_name + ". To stop enter stop: ")
         else:
             parameters_values[parameter_name] = input("Enter value for " + parameter_name + ": ")
     return connection.execute_method(method_name, parameters_values)
 
 
-EXIT_METHOD_TEXT = 'exit'
-SHOW_METHODS_TEXT = 'show_methods'
-LOAD_CONFIG_FILE_TEXT = "load_config_file"
-EXPORT_CONFIG_FILE_TEXT = "export_config_file"
-
 connection = zerorpc.Client()
-connection.connect("tcp://0.0.0.0:4001")
+# endpoint = input("Enter a zerorpc endpoint to connect to: ")
+endpoint = "tcp://0.0.0.0:4001"
+connection.connect(endpoint)
+try:
+    print("Connecting to", endpoint + "...")
+    if connection.check_connection():
+        print("Connected successfully")
+except zerorpc.LostRemote:
+    sys.exit("Unable to connect to " + endpoint)
 
 methods = connection.get_methods()
 methods.insert(0, EXIT_METHOD_TEXT)
 methods.insert(1, SHOW_METHODS_TEXT)
 methods.insert(2, LOAD_CONFIG_FILE_TEXT)
 methods.insert(3, EXPORT_CONFIG_FILE_TEXT)
+methods.insert(4, CLEAR_CONSOLE_TEXT)
 print(methods)
-user_input = input("Enter method to execute for cli: ")
+user_input = input("Enter method to execute: ")
 while user_input != EXIT_METHOD_TEXT:
     if user_input == SHOW_METHODS_TEXT:
         print(methods)
     elif user_input == LOAD_CONFIG_FILE_TEXT:
         file_path = input("Enter the config file path: ")
-        print("Loading config")
         try:
             with open(file_path) as config_file:
-                # /home/internet/Desktop/components.yaml /home/internet/Desktop/packages
+                # /home/internet/Desktop/components.yaml
                 components = yaml.load(config_file, Loader=yaml.FullLoader)
-            connection.execute_method("setup_components", {"components": components})
+            print(connection.execute_method("setup_components", {"components": components}))
         except FileNotFoundError as error:
             print(error.args[1], "'{}'".format(file_path))
         except IsADirectoryError as error:
@@ -58,6 +75,8 @@ while user_input != EXIT_METHOD_TEXT:
                                   execute_method("get_pipeline_creation", {}),
                                   config_file)
             print(documents)
+    elif user_input == CLEAR_CONSOLE_TEXT:
+        clear_console()
     elif user_input in methods:
         print(execute_method(user_input, connection))
-    user_input = input("Enter method to execute for cli: ")
+    user_input = input("Enter method to execute : ")
