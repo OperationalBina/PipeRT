75567b47730860f6740626c03150d3db5369cf36
vlad <Vladi.shtompel@gmail.com>
2020-05-09 18:31:15 +0300

    trying something stupid

--- pipert/core/main.py
+++ pipert/core/main.py
@@ -26,58 +26,61 @@ class CliConnection(object):
         return True
 
 
-pipeline_manager = PipelineManager()
+if __name__ == "__main__":
+    import torch
+    torch.multiprocessing.set_start_method('spawn', force=True)
+    pipeline_manager = PipelineManager()
 
-if not os.environ.get("UI", "").lower() == 'true':
-    cli_server = zerorpc.Server(CliConnection(pipeline_manager))
-    cli_server.bind("tcp://0.0.0.0:" + os.environ.get("CLI_ENDPOINT", "4001"))
-    cli_server.run()
-else:
-    app = Flask(__name__)
+    if not os.environ.get("UI", "").lower() == 'true':
+        cli_server = zerorpc.Server(CliConnection(pipeline_manager))
+        cli_server.bind("tcp://0.0.0.0:" + os.environ.get("CLI_ENDPOINT", "4001"))
+        cli_server.run()
+    else:
+        app = Flask(__name__)
 
-    def return_response(res_object):
-        return Response(res_object["Message"], 200 if res_object["Succeeded"] else 400)
+        def return_response(res_object):
+            return Response(res_object["Message"], 200 if res_object["Succeeded"] else 400)
 
-    @app.route("/routines", methods=['GET'])
-    def get_routines():
-        return jsonify(pipeline_manager.get_all_routine_types())
+        @app.route("/routines", methods=['GET'])
+        def get_routines():
+            return jsonify(pipeline_manager.get_all_routine_types())
 
-    @app.route("/routineParams/<routine_name>", methods=['GET'])
-    def get_routine_params(routine_name):
-        return jsonify(pipeline_manager.get_routine_parameters(routine_name))
+        @app.route("/routineParams/<routine_name>", methods=['GET'])
+        def get_routine_params(routine_name):
+            return jsonify(pipeline_manager.get_routine_parameters(routine_name))
 
-    @app.route("/component", methods=['GET'])
-    def get_component():
-        return "TBD"
+        @app.route("/component", methods=['GET'])
+        def get_component():
+            return "TBD"
 
-    @app.route("/pipeline", methods=['POST', 'GET'])
-    def create_pipeline():
-        if request.method == 'GET':
-            return jsonify(
-                [
-                    {
-                        "name": "name of component",
-                        "queues": "[array of names of queues]",
-                        "routines":
-                            [
-                                {
-                                    "routine_type_name": "name of the routine type",
-                                    "routine_param_name": "the values of the routine parameters that can be fount in"
-                                                          "/routineParams/routineTypeName"
-                                }
-                            ]
-                    }
-                ]
-            )
-        elif request.method == 'POST':
-            return return_response(pipeline_manager.setup_components(request.json))
+        @app.route("/pipeline", methods=['POST', 'GET'])
+        def create_pipeline():
+            if request.method == 'GET':
+                return jsonify(
+                    [
+                        {
+                            "name": "name of component",
+                            "queues": "[array of names of queues]",
+                            "routines":
+                                [
+                                    {
+                                        "routine_type_name": "name of the routine type",
+                                        "routine_param_name": "the values of the routine parameters that can be fount in"
+                                                              "/routineParams/routineTypeName"
+                                    }
+                                ]
+                        }
+                    ]
+                )
+            elif request.method == 'POST':
+                return return_response(pipeline_manager.setup_components(request.json))
 
-    @app.route("/kill", methods=['PUT'])
-    def stop_components():
-        return return_response(pipeline_manager.stop_all_components())
+        @app.route("/kill", methods=['PUT'])
+        def stop_components():
+            return return_response(pipeline_manager.stop_all_components())
 
-    @app.route("/run", methods=['PUT'])
-    def start_components():
-        return_response(pipeline_manager.run_all_components())
+        @app.route("/run", methods=['PUT'])
+        def start_components():
+            return_response(pipeline_manager.run_all_components())
 
-    app.run(port=os.environ.get("UI_PORT", 5005), host='0.0.0.0')
+        app.run(port=os.environ.get("UI_PORT", 5005), host='0.0.0.0')
