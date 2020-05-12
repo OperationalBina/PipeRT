5bf60fbd13dd8efaf9df5ca4c0298adcb15c7ad2
vlad <Vladi.shtompel@gmail.com>
2020-05-12 20:49:29 +0300

    video pacing fixed

--- pipert/contrib/routines/listen_to_stream.py
+++ pipert/contrib/routines/listen_to_stream.py
@@ -20,8 +20,11 @@ class ListenToStream(Routine):
         self.is_file = str(stream_address).endswith("mp4")
         self.stream = None
         self.q_handler = QueueHandler(queue)
-        self.fps = fps
         self.updated_config = {}
+        if self.is_file:
+            self.sma = SimpleMovingAverage(value=0.1, count=19)
+            self.ts = time.time()
+        self.fps = fps
 
     def begin_capture(self):
         self.stream = cv2.VideoCapture(self.stream_address)
@@ -65,6 +68,11 @@ class ListenToStream(Routine):
             msg.update_payload(frame)
 
             success = self.q_handler.deque_non_blocking_put(msg)
+            if self.is_file:
+                delta = time.time() - self.ts
+                self.sma.add(delta)
+                time.sleep(max(0, (1.0 - self.sma.current * self.fps) / self.fps))
+                self.ts = time.time()
             return success
 
     def setup(self, *args, **kwargs):
@@ -86,3 +94,20 @@ class ListenToStream(Routine):
 
     def does_routine_use_queue(self, queue):
         return self.q_handler.q == queue
+
+
+class SimpleMovingAverage(object):
+    ''' Simple moving average '''
+    def __init__(self, value=0.0, count=7):
+        self.count = int(count)
+        self.current = float(value)
+        self.samples = [self.current] * self.count
+
+    def __str__(self):
+        return str(round(self.current, 3))
+
+    def add(self, value):
+        v = float(value)
+        self.samples.insert(0, v)
+        o = self.samples.pop()
+        self.current = self.current + (v-o)/self.count
