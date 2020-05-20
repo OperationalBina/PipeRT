4aef0049ae97f702b79abef8686af01ec7f42dbf
MayoG <dhtnthu9794048@gmail.com>
2020-05-20 17:16:20 +0300

    Fix bugs in component_factory

--- pipert/core/component_factory.py
+++ pipert/core/component_factory.py
@@ -17,20 +17,20 @@ def open_config_file(config_path) -> Union[str, dict]:
         with open(config_path) as config_file:
             return yaml.load(config_file, Loader=yaml.FullLoader)
     except FileNotFoundError as error:
-        error_msg = (error.args[1], "'{}'".format(config_path))
+        error_msg = error.args[1] + "' {}'".format(config_path)
     except IsADirectoryError as error:
-        error_msg = ("'{}' is a directory not a file".format(config_path))
+        error_msg = "'{}' is a directory not a file".format(config_path)
     except (ScannerError, ParserError):
-        error_msg = ("Expecting yaml file, can't parse the file '{}'".format(config_path))
+        error_msg = "Expecting yaml file, can't parse the file '{}'".format(config_path)
     return error_msg
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-cp', '--config_path', help='Configuration file path', type=str,
-                        default='./config_file.yaml')
+                        default='pipert/core/config.yaml')
     parser.add_argument('-p', '--port', help='ZeroRPC port', type=str, default=None)
-    opts = parser.parse_args()
+    opts, unknown = parser.parse_known_args()
 
     if opts.port is None:
         exit("Must get port in the script parameters")
@@ -39,10 +39,10 @@ if __name__ == '__main__':
 
     if isinstance(component_config, str):
         exit(component_config)
-
+    print (component_config)
     component_factory = ClassFactory(COMPONENTS_FOLDER_PATH)
 
-    _, component_params = component_config.items()[0]
+    _, component_params = list(component_config.items())[0]
 
     if "component_type_name" in component_params:
         component_class = component_factory.get_class(component_params["component_type_name"])
