8ecd91d2c6b536c3f0e3a85c15359f76d08802e8
AmitKarin <amitkarin9@gmail.com>
2020-05-25 13:41:39 +0300

    added the monitoring_mode argument to the base func - create component and create premade component

--- pipert/core/pipeline_manager.py
+++ pipert/core/pipeline_manager.py
@@ -46,16 +46,16 @@ class PipelineManager:
         self.COMPONENTS_FOLDER_PATH = "pipert/contrib/components"
 
     @component_name_existence_error(need_to_be_exist=False)
-    def create_component(self, component_name, use_shared_memory=False):
+    def create_component(self, component_name, use_shared_memory=False, monitoring_mode=NullCollector()):
         self.components[component_name] = \
-            BaseComponent(name=component_name, use_memory=use_shared_memory)
+            BaseComponent(name=component_name, use_memory=use_shared_memory, monitoring_mode=monitoring_mode)
         return self._create_response(
             True,
             f"Component {component_name} has been created"
         )
 
     @component_name_existence_error(need_to_be_exist=False)
-    def create_premade_component(self, component_name, component_type_name, use_shared_memory=False):
+    def create_premade_component(self, component_name, component_type_name, use_shared_memory=False, monitoring_mode=NullCollector()):
         component_class = \
             self._get_component_class_object_by_type_name(component_type_name)
         if component_class is None:
@@ -64,7 +64,7 @@ class PipelineManager:
                 f"The component type {component_type_name} doesn't exist"
             )
         self.components[component_name] = \
-            component_class(name=component_name, use_memory=use_shared_memory)
+            component_class(name=component_name, use_memory=use_shared_memory, monitoring_mode=monitoring_mode)
         return self._create_response(
             True,
             f"Component {component_name} has been created"
