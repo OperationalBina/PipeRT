a91063833bb113be0776e802929e16a394cdc3c5
mayog <dhtnthu9794048@gmail.com>
2020-06-23 12:15:38 +0300

    Add new tests for component factory script

---
+++ tests/pipert/core/test_component_factory.py
@@ -0,0 +1,50 @@
+from time import sleep
+import os
+import signal
+import subprocess
+import pytest
+
+COMPONENT_FACTORY_FILE_PATH = "pipert/core/component_factory.py"
+DUMMY_COMPONENT_CONFIG_PATH = 'tests/pipert/core/utils/dummy_component_config.yaml'
+COMPONENT_PORT = "5050"
+
+
+def test_bad_config_path():
+    bad_config_path = 'tests/pipert/core/utils/bad_path.yaml'
+    cmd = "python " + COMPONENT_FACTORY_FILE_PATH + " -cp " + bad_config_path + " -p " + COMPONENT_PORT
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    process_is_running = does_process_running(process)
+    if process_is_running:
+        kill_process(process)
+        assert not process_is_running, "Expected to get an error"
+
+    assert process.returncode
+
+
+def test_no_port_sent():
+    cmd = "python " + COMPONENT_FACTORY_FILE_PATH + " -cp " + DUMMY_COMPONENT_CONFIG_PATH
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    process_is_running = does_process_running(process)
+    if process_is_running:
+        kill_process(process)
+        assert not process_is_running, "Expected to get an error"
+
+    _, error = process.communicate()
+    assert process.returncode
+    assert error == "Must get port for the zeroRPC server in the script parameters"
+
+
+def test_good_parameters():
+    cmd = "python " + COMPONENT_FACTORY_FILE_PATH + " -cp " + DUMMY_COMPONENT_CONFIG_PATH + " -p " + COMPONENT_PORT
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    sleep(3)
+    assert does_process_running(process)
+    kill_process(process)
+
+
+def does_process_running(process):
+    return process.poll() is None
+
+
+def kill_process(process):
+    os.killpg(os.getpgid(process.pid), signal.SIGTERM)  # Send the signal to all the process groups
