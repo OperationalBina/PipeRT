294e27b55ad9ad3a83360b53dab0adff5db54378
mayog <dhtnthu9794048@gmail.com>
2020-07-02 15:15:57 +0300

    fix the logger issues with tests

--- tests/pipert/core/utils/routines/dummy_routine.py
+++ tests/pipert/core/utils/routines/dummy_routine.py
@@ -1,7 +1,5 @@
 import logging
-from collections import defaultdict
-import multiprocessing as mp
-from pipert.core.metrics_collector import NullCollector
+
 from pipert.core.routine import Routine
 
 
@@ -13,20 +11,10 @@ class DummyRoutine(Routine):
     def does_routine_use_queue(self, queue):
         return False
 
-    def __init__(self, name="", component_name="", metrics_collector=NullCollector(), *args, **kwargs):
-        self.name = name
-        # name of the component that instantiated the routine
-        self.component_name = component_name
-        self.metrics_collector = metrics_collector
-        self.use_memory = False
-        self.generator = None
-        self.stop_event: mp.Event = None
-        self._event_handlers = defaultdict(list)
-        self.state = None
-        self._allowed_events = []
-        self.runner = None
-        self.runner_creator = None
-        self.runner_creator_kwargs = {}
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _setup_logger(self):
         self.logger = logging.getLogger("test_logs.log")
 
     def main_logic(self, *args, **kwargs):
--- tests/pipert/core/utils/routines/dummy_routine_with_queue.py
+++ tests/pipert/core/utils/routines/dummy_routine_with_queue.py
@@ -1,26 +1,14 @@
 import logging
-from collections import defaultdict
-import multiprocessing as mp
-from pipert.core.metrics_collector import NullCollector
+
 from pipert.core.routine import Routine
 
 
 class DummyRoutineWithQueue(Routine):
-    def __init__(self, queue, name="", component_name="", metrics_collector=NullCollector(), *args, **kwargs):
-        self.name = name
-        # name of the component that instantiated the routine
-        self.component_name = component_name
-        self.metrics_collector = metrics_collector
-        self.use_memory = False
-        self.generator = None
-        self.stop_event: mp.Event = None
-        self._event_handlers = defaultdict(list)
-        self.state = None
-        self._allowed_events = []
-        self.runner = None
-        self.runner_creator = None
-        self.runner_creator_kwargs = {}
+    def __init__(self, queue, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.queue = queue
+
+    def _setup_logger(self):
         self.logger = logging.getLogger("test_logs.log")
 
     def main_logic(self, *args, **kwargs):
