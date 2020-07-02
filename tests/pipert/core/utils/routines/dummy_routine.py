213f5df63b8543126fe94742fffc47375102d05d
mayog <dhtnthu9794048@gmail.com>
2020-07-02 14:54:28 +0300

    Change test routines to not use the logger

--- tests/pipert/core/utils/routines/dummy_routine.py
+++ tests/pipert/core/utils/routines/dummy_routine.py
@@ -1,3 +1,6 @@
+from collections import defaultdict
+
+from pipert.core.metrics_collector import NullCollector
 from pipert.core.routine import Routine
 
 
@@ -9,8 +12,19 @@ class DummyRoutine(Routine):
     def does_routine_use_queue(self, queue):
         return False
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, name="", component_name="", metrics_collector=NullCollector(), *args, **kwargs):
+        self.name = name
+        # name of the component that instantiated the routine
+        self.component_name = component_name
+        self.metrics_collector = metrics_collector
+        self.use_memory = False
+        self.generator = None
+        self._event_handlers = defaultdict(list)
+        self.state = None
+        self._allowed_events = []
+        self.runner = None
+        self.runner_creator = None
+        self.runner_creator_kwargs = {}
 
     def main_logic(self, *args, **kwargs):
         self.metrics_collector.collect_latency(0.1, self.component_name)
--- tests/pipert/core/utils/routines/dummy_routine_with_queue.py
+++ tests/pipert/core/utils/routines/dummy_routine_with_queue.py
@@ -1,9 +1,23 @@
+from collections import defaultdict
+
+from pipert.core.metrics_collector import NullCollector
 from pipert.core.routine import Routine
 
 
 class DummyRoutineWithQueue(Routine):
-    def __init__(self, queue, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, queue, name="", component_name="", metrics_collector=NullCollector(), *args, **kwargs):
+        self.name = name
+        # name of the component that instantiated the routine
+        self.component_name = component_name
+        self.metrics_collector = metrics_collector
+        self.use_memory = False
+        self.generator = None
+        self._event_handlers = defaultdict(list)
+        self.state = None
+        self._allowed_events = []
+        self.runner = None
+        self.runner_creator = None
+        self.runner_creator_kwargs = {}
         self.queue = queue
 
     def main_logic(self, *args, **kwargs):
