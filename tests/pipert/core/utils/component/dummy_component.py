dd159944bca84a9209907a4495407429e11111fd
Elon-Abulafia <elon945@gmail.com>
2020-11-23 10:51:40 +0200

    Add dummy logger

--- tests/pipert/core/utils/component/dummy_component.py
+++ tests/pipert/core/utils/component/dummy_component.py
@@ -1,10 +1,14 @@
 from pipert.core.component import BaseComponent
+import logging
 
 
 class DummyComponent(BaseComponent):
 
     def __init__(self, component_config, start_component=False):
         super().__init__(component_config, start_component=start_component)
+
+    def _setup_logger(self):
+        self.logger = logging.getLogger("test_logs.log")
 
     @staticmethod
     def add(a, b):
