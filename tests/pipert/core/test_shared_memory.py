0a645f197284ec8426d475c21ceb4e1745d92dd9
Elon-Abulafia <60340809+Elon-Abulafia@users.noreply.github.com>
2021-02-08 10:50:53 +0200

    Create test_shared_memory.py
    Fix tests

--- tests/pipert/core/test_shared_memory.py
+++ tests/pipert/core/test_shared_memory.py
@@ -8,17 +8,14 @@ class DummySharedMemoryGenerator(sm.SharedMemoryGenerator):
 
 def test_cleanup():
     generator = DummySharedMemoryGenerator()
-    generator.get_next_shared_memory()
-    generator.get_next_shared_memory()
     generator.cleanup()
     assert generator.shared_memories == {}
 
 
 def test_get_next_shared_memory():
     generator = DummySharedMemoryGenerator()
-    first_memory = generator.get_next_shared_memory()
-    second_memory = generator.get_next_shared_memory()
-    assert first_memory != second_memory
+    first_memory = generator.get_next_shared_memory_name()
+    second_memory = generator.get_next_shared_memory_name()
     assert first_memory == "dummy_component_0"
     assert second_memory == "dummy_component_1"
     generator.cleanup()
@@ -26,17 +23,17 @@ def test_get_next_shared_memory():
 
 def test_max_count():
     generator = DummySharedMemoryGenerator()
-    first_memory = generator.get_next_shared_memory()
+    first_memory = generator.get_next_shared_memory_name()
     for _ in range(generator.max_count - 1):
-        generator.get_next_shared_memory()
+        generator.get_next_shared_memory_name()
 
-    assert first_memory == generator.get_next_shared_memory()
+    assert first_memory == generator.get_next_shared_memory_name()
     generator.cleanup()
 
 
 def test_write_and_read_from_memory():
     generator = DummySharedMemoryGenerator()
-    memory_name = generator.get_next_shared_memory()
+    memory_name = generator.get_next_shared_memory_name()
     memory = generator.get_shared_memory_object(memory_name)
     memory.acquire_semaphore()
     memory.write_to_memory(b"AAA")
