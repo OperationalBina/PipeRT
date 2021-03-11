fac1c568632820e4395b2953a7d21e01369a5762
MayoG <dhtnthu9794048@gmail.com>
2021-03-11 10:45:20 +0200

    Create memories only with posix

--- pipert/core/component.py
+++ pipert/core/component.py
@@ -99,7 +99,7 @@ class BaseComponent:
         """
         self.logger.info("Running component")
         self.stop_event.clear()
-        if self.use_memory:
+        if self.use_memory and sys.version_info.minor < 8:
             self.generator.create_memories()
         self._start()
         gevent.signal_handler(signal.SIGTERM, self.stop_run)
